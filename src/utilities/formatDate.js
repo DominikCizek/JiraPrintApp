e7acd1d95d88d4fccc98832e44505df5d231ad4a
DominikCizek <62253739+DominikCizek@users.noreply.github.com>
2022-12-27 08:45:21 +0100

    app completed

--- src/utilities/formatDate.js
+++ src/utilities/formatDate.js
@@ -2,5 +2,5 @@ export function formatDate(string){
     const rawDate = new Date(string);
     let days = ['Ne', 'Po', 'Út', 'St', 'Čt', 'Pá', 'So'];
     let dayName = days[rawDate.getDay()];
-    return `${dayName} - ${rawDate.getDay()}. ${rawDate.getMonth()}.`
+    return `${dayName} - ${rawDate.getDate()}.${rawDate.getMonth()+1}.`
 }
